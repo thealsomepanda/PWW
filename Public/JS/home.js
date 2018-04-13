7f574a80e175eacbacbf2970fc10df1c4e6bb943
thealsomepanda <thealsomepanda@gmail.com>
2018-04-12 22:38:19 -0400

    commit

--- Public/JS/home.js
+++
@@ -1,7 +0,0 @@
-const emailButton = document.getElementById('emailButton');
-const myHeader = document.getElementById('myHeader');
-const emailIn = document.getElementById('emailIn');
-
-emailButton.addEventListener('click', () => {
-    myHeader.style.color = emailIn.value;
-});
