17bb354364ae7eb922f762878be758e9d71f5867
thealsomepanda <thealsomepanda@gmail.com>
2018-04-18 10:10:34 -0400

    commit

--- Public/JS/js/index(home).js
+++ Public/JS/js/index(home).js
@@ -1,1 +1,7 @@
-alert('Hello');
+const userEmail = document.getElementsByName('emailSignup');
+const emailButton = document.getElementsByName('emailButton');
+
+emailButton.addEventListener('click', () => {
+    userEmail.value = userEmail.value;
+    //alert(userEmail);
+});
