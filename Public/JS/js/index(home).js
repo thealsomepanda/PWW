3a7b533bb692349683b79b1786c692b8e4fa0439
thealsomepanda <thealsomepanda@gmail.com>
2018-04-23 11:31:24 -0400

    commit

--- Public/JS/js/index(home).js
+++ Public/JS/js/index(home).js
@@ -4,5 +4,11 @@ let userEmailAssignment;
 
 emailButton.addEventListener('click', () => {
     userEmailAssignment = userEmail.value;
+
+    let re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    let validEmail = re.test(userEmailAssignment);
+    console.log(validEmail);
+
 });
 
